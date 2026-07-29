4c95a99d503a35e7f22a8ce92df71d9042c88288
FredericoGSilva <frederico.dev@outlook.com>
2026-07-29 01:38:35 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,7 +9,6 @@ public class Board {
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
-        // matriz de paças, instanciada com o número de linhas e colunas do tabuleiro
         pieces = new Piece[rows][columns];
     }
 
@@ -27,6 +26,14 @@ public class Board {
 
     public void setColumns(int columns) {
         this.columns = columns;
+    }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
     }
     
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,19 +1,15 @@
 package boardgame;
 
 public class Piece {
-    
-    // Posição simples de matriz e não de de xadrez
+
     protected Position position;
     private Board board;
 
     public Piece(Board board) {
         this.board = board;
-        // A peça não foi colocada em nenhuma posição ainda
         position = null;
     }
-    
-    // apenas classes do mesmo pacote ou subclasses podem acessar o tabuleiro
-    // Tabuleiro associado a uma peça será acessado apenas por classes do mesmo pacote ou por subclasses de Piece 
+
     protected Board getBoard() {
         return board;
     }
